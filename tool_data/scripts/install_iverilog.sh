0e0db421cf13d0c47b67aa681c3c76fe3bd24efd
Jose Cueva <joseluis7012@gmail.com>
2018-12-03 04:20:52 -0700

    installing iverilgo

---
+++ tool_data/scripts/install_iverilog.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#remove existent iverilog installations
+yes|sudo apt-get purge iverilog
+yes|sudo apt-get install flex
+yes|sudo apt-get install gcc
+yes|sudo apt-get install bison
+yes|sudo apt-get install g++
+yes|sudo apt-get install autoconf
+
+
+mkdir -p $HOME/git_folder
+cd $HOME/git_folder
+git clone https://github.com/steveicarus/iverilog
+cd iverilog/
+./configure
+sudo make
+sudo make install
