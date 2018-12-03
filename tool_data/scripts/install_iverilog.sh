5d870ca607c2d8160179f43d3b20aefa34ac97b5
Jose Cueva <joseluis7012@gmail.com>
2018-12-03 04:27:19 -0700

    installing iverilog

--- tool_data/scripts/install_iverilog.sh
+++ tool_data/scripts/install_iverilog.sh
@@ -6,12 +6,13 @@ yes|sudo apt-get install gcc
 yes|sudo apt-get install bison
 yes|sudo apt-get install g++
 yes|sudo apt-get install autoconf
-
+yes|sudo apt-get install gperf
 
 mkdir -p $HOME/git_folder
 cd $HOME/git_folder
 git clone https://github.com/steveicarus/iverilog
 cd iverilog/
+autoconf
 ./configure
 sudo make
 sudo make install
